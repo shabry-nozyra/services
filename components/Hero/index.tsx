511b1ba0a429dd51dd559f90e84d28591ae7acb4
Aria Shabry <32570516+Ariashabry@users.noreply.github.com>
2025-05-19 10:33:04 +0700

    feat: new initiate project

---
+++ components/Hero/index.tsx
@@ -0,0 +1,92 @@
+"use client";
+import Image from "next/image";
+import { useState } from "react";
+
+const Hero = () => {
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <section className="overflow-hidden pb-20 pt-35 md:pt-40 xl:pb-25 xl:pt-46">
+        <div className="mx-auto max-w-c-1390 px-4 md:px-8 2xl:px-0">
+          <div className="flex lg:items-center lg:gap-8 xl:gap-32.5">
+            <div className=" md:w-1/2">
+              <h4 className="mb-4.5 text-lg font-medium text-black dark:text-white">
+                NOZYRA IT Solutions
+              </h4>
+              <h1 className="mb-5 pr-16 text-4xl font-bold text-black dark:text-white xl:text-6xl ">
+                Built to Do What's
+                <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark ">
+                  Next
+                </span>
+              </h1>
+              <p>
+                Find new opportunities. Create experiences your customers actually care about. Take control of your future. It all happens in the Marketplace designed around you.
+              </p>
+
+              <div className="mt-10">
+                <a href="#" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-meta hover:bg-meta focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+                  Get explore services
+                  <svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                </a>
+                <a href="#" className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                  Speak to Sales
+                </a>
+{/* 
+                <p className="mt-5 text-black dark:text-white">
+                  Try for free no credit card required.
+                </p> */}
+              </div>
+            </div>
+
+            <div className="animate_right hidden md:w-1/2 lg:block">
+              <div className="relative 2xl:-mr-7.5">
+                <Image
+                  src="/images/shape/shape-01.png"
+                  alt="shape"
+                  width={46}
+                  height={246}
+                  className="absolute -left-11.5 top-0"
+                />
+                <Image
+                  src="/images/shape/shape-02.svg"
+                  alt="shape"
+                  width={36.9}
+                  height={36.7}
+                  className="absolute bottom-0 right-0 z-10"
+                />
+                <Image
+                  src="/images/shape/shape-03.svg"
+                  alt="shape"
+                  width={21.64}
+                  height={21.66}
+                  className="absolute -right-6.5 bottom-0 z-1"
+                />
+                <div className=" relative aspect-700/444 w-full">
+                  <Image
+                    className="shadow-solid-l dark:hidden"
+                    src="/images/hero/hero-light.svg"
+                    alt="Hero"
+                    fill
+                  />
+                  <Image
+                    className="hidden shadow-solid-l dark:block"
+                    src="/images/hero/hero-dark.svg"
+                    alt="Hero"
+                    fill
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Hero;
